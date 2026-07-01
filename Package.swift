c0aefc960f0d2669a9ccb9943852e40555c7fd0b
Carl Peto <carlpeto@Carls-MacBook-Pro.local>
2026-07-01 12:05:17 +0100

    license headers

--- Package.swift
+++ Package.swift
@@ -1,4 +1,17 @@
 // swift-tools-version: 6.2
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
--- verify-undname-mappings.py
+++ verify-undname-mappings.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift.org open source project
+##
+## Copyright (c) 2025 Apple Inc. and the Swift project authors
+## Licensed under Apache License v2.0 with Runtime Library Exception
+##
+## See https://swift.org/LICENSE.txt for license information
+## See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+##
+##===----------------------------------------------------------------------===##
+
 """Verify Swift MSVC demangler unit-test expectations against undname.exe.
 
 The Swift unit tests in Tests/swift-symbolicateTests/msvcDemanglerTests.swift
